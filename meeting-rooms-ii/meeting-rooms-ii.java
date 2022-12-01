09f591906e90165337ed41c3de52d590570bceeb
Podugu Rohith Surya <prsurya1020@gmail.com>
2022-11-30 22:35:57 -0800

    Time: 12 ms, Memory: 45.4 MB - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.java
+++ meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,28 +1,17 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
         Arrays.sort(intervals, (a,b)->a[0]-b[0]);
-        List<List<int[]>> lists = new ArrayList<>();
-        boolean found=false;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int rooms=0;
         for(int[] interval: intervals) {
-            int te = lists.size();
-            for(int i=0; i<te; i++) {
-                found=true;
-                for(int[] t: lists.get(i)) {
-                    if(((t[0]<=interval[0] && interval[0]<t[1]) || (t[0]<=interval[1] && interval[1]<t[1]))) {
-                        found=false;
-                        break;
-                    }
-                }
-                if(found) {
-                    lists.get(i).add(interval);
-                    break;
-                }
-            }
-            if(!found) {
-                lists.add(new ArrayList<>());
-                lists.get(lists.size()-1).add(interval);
+            if(pq.size()==0 || pq.peek()>interval[0]) {
+                pq.offer(interval[1]);
+                rooms++;
+            } else {
+                int top = pq.poll();
+                pq.offer(interval[1]);
             }
         }
-        return lists.size();
+        return rooms;
     }
 } 
