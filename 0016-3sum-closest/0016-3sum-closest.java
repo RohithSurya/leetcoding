995167f6acafc8cf0d35ee26635c458f71a0d9f1
Podugu Rohith Surya <prsurya1020@gmail.com>
2022-12-15 18:13:50 -0800

    Time: 320 ms (9.00%), Space: 42.1 MB (83.68%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,74 @@
+class Solution {
+    public int threeSumClosest(int[] a, int target) {
+        Arrays.sort(a);
+        int n=a.length;
+        int csum=9999999;
+        for(int i=0; i<n-1; i++) {
+            int temp = twoSum(a, i, target);
+            if(Math.abs(temp-target)<Math.abs(csum-target)) {
+                csum=temp;
+            }
+        }
+        return csum;
+    }
+    
+    public int twoSum(int[] a, int index, int target) {
+        int gsum=9999999;
+        for(int i=index+1; i<a.length; i++) {
+            int temp = target - (a[index]+a[i]);
+            int fIndex = binarySearchGreaterOrEqual(a, temp, i);
+            int sum=0;
+            int fsum=0, rsum=0;
+            if(fIndex<a.length) {
+                if(temp==a[fIndex]) return target;
+                fsum = a[index]+a[i]+a[fIndex];
+                // System.out.println(fsum);
+                gsum = Math.abs(gsum-target) > Math.abs(fsum-target) ? fsum: gsum;
+            }
+            int rIndex = binarySearchLessOrEqual(a,temp, i);
+            if(rIndex>i) {
+                if(temp==a[rIndex]) return target;
+                rsum = a[index]+a[i]+a[rIndex];
+                // System.out.println(index);
+                // System.out.println(i);
+                // System.out.println(rIndex);
+                // System.out.println("");
+                // System.out.println(rsum);
+                // System.out.println(gsum);
+                // System.out.println("");
+                gsum = Math.abs(gsum-target) > Math.abs(rsum-target)  ? rsum: gsum;
+            }
+        }
+        return gsum;
+    } 
+    
+    public int binarySearchGreaterOrEqual(int[] a, int target, int start) {
+        int n = a.length;
+        int low=start+1;
+        int high=n-1;
+        int mid;
+        while(low<high) {
+            mid=(low+high)/2;
+            if(a[mid]<target)
+                low=mid+1;
+            else
+                high=mid;
+        }
+        return low;
+    }
+
+    public int binarySearchLessOrEqual(int[] a, int target, int start) {
+        int n = a.length;
+        int low=start+1;
+        int high=n-1;
+        int mid;
+        while(low<=high) {
+            mid=(low+high)/2;
+            if(a[mid]>target) {
+                high=mid-1;
+            } else
+                low=mid+1;
+        }
+        return high;
+    }
+}
