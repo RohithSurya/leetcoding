47cd5b046bd730bbd51df6a834c702f9316d35bb
Podugu Rohith Surya <prsurya1020@gmail.com>
2023-01-01 19:56:39 -0800

    Time: 653 ms (5.09%), Space: 45.5 MB (90.73%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -2,28 +2,18 @@ class Solution {
     Map<Integer, List<Pair<Integer, Integer>>> adj = new HashMap<>();
     
     
-    public void dijtras(int[] sRecv, int k, int n) {
-        Queue<Pair<Integer, Integer>> q = new PriorityQueue<>(Comparator.comparing(Pair::getKey));
-        sRecv[k]=0;
-        q.offer(new Pair(0, k));
-        while(!q.isEmpty()) {
-            Pair<Integer, Integer> tp = q.remove();
-            int currNode = tp.getValue();
-            int currNodeValue = tp.getKey();
-            if(sRecv[currNode]<currNodeValue) continue;
-            if(!adj.containsKey(currNode)) continue;
-            
-            for(var adjNodes: adj.get(currNode)) {
-                int dest = adjNodes.getKey();
-                int weight = adjNodes.getValue();
-                if(currNodeValue+weight<sRecv[dest]) {
-                    sRecv[dest] = weight+currNodeValue;
-                    q.offer(new Pair(sRecv[dest], dest));
-                }
-            }
+    public void dfs(int[] sRecv, int k, int time) {
+        if(time>=sRecv[k]) {
+            return;
         }
+        sRecv[k]=time;
         
-        
+        if(!adj.containsKey(k)) return;
+        for(var pa: adj.get(k)) {
+            var dest = pa.getKey();
+            var weight = pa.getValue();
+            dfs(sRecv, dest, time+weight);
+        }
     }
     public int networkDelayTime(int[][] times, int n, int k) {
         for(int[] time: times) {
@@ -41,7 +31,7 @@ class Solution {
         
         int[] sRecv = new int[n+1];
         Arrays.fill(sRecv, Integer.MAX_VALUE);
-        dijtras(sRecv, k, n);
+        dfs(sRecv, k, 0);
         int minTime = Integer.MIN_VALUE;
         for(int i=1; i<sRecv.length; i++) {
             minTime = Math.max(sRecv[i], minTime);
