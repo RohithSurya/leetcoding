1e61c7299a2d6ee28d6d424c9e2187dfd903f225
Podugu Rohith Surya <prsurya1020@gmail.com>
2023-02-28 17:26:53 -0800

    Time: 43 ms (11.03%), Space: 50.8 MB (11.85%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,17 +1,18 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length()][text2.length()];
+        int m=text1.length(), n = text2.length();
+        int[][] dp = new int[m][n];
         for(var row: dp)
             Arrays.fill(row, -1);
-        return lcs(text1, text2, 0, 0, dp);
+        return lcs(text1, text2, 0, 0, dp, m, n);
     }
-    public int lcs(String s1, String s2, int i, int j, int[][] dp) {
-        if(s1.length()==i || s2.length()==j) return 0;
+    public int lcs(String s1, String s2, int i, int j, int[][] dp, int m, int n) {
+        if(m==i || n==j) return 0;
         else if(dp[i][j]!=-1) return dp[i][j];
         else if(s1.charAt(i)==s2.charAt(j)) {
-            dp[i][j] = 1+lcs(s1, s2, i+1, j+1, dp);
+            dp[i][j] = 1+lcs(s1, s2, i+1, j+1, dp, m, n);
         } else {
-             dp[i][j] = Math.max(lcs(s1, s2, i, j+1,dp), lcs(s1, s2, i+1, j, dp));
+             dp[i][j] = Math.max(lcs(s1, s2, i, j+1,dp, m, n), lcs(s1, s2, i+1, j, dp, m, n));
         }
         return dp[i][j];
     }
