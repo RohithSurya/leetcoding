727c4a3dfaf0f2fa614fe86be520b353fbeb3cc1
Podugu Rohith Surya <prsurya1020@gmail.com>
2023-05-30 19:27:26 -0700

    Time: 1 ms (78.57%), Space: 39.9 MB (99.64%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int i=0;
+        boolean fullBreak=false;
+        while(true) {
+            char prev = 0;
+            for(String str: strs) {
+                if(i>=str.length()) {
+                    fullBreak = true; break;
+                }
+                if(prev==0) prev=str.charAt(i);
+                else if(prev!=str.charAt(i)) {
+                    fullBreak = true;
+                    break;
+                }
+            }
+            if(fullBreak) break;
+            i++;
+        }
+        if(i==0) return "";
+        return strs[0].substring(0,i);
+    }
+}
