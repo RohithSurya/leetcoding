50314d41cf69c4dd5b012175dd0e092d9c7a7658
Podugu Rohith Surya <prsurya1020@gmail.com>
2024-02-20 14:46:14 -0800

    Time: 556 ms, Memory: 60.3 MB - LeetHub

--- 0min-cost-to-connect-all-points/0min-cost-to-connect-all-points.java
+++ 0min-cost-to-connect-all-points/0min-cost-to-connect-all-points.java
@@ -1,64 +1,36 @@
 class Solution {
     
-    int[] root;
-    int[] rank;
-    int components;
-    
-    public void initialize(int size) {
-        root = new int[size];
-        rank = new int[size];
-
-        for(int i=0; i<root.length; i++) {
-            root[i]=i;
-            rank[i]=i;
-        }
-    }
-    
-    public int find(int x) {
-        if(x==root[x]) return x;
-        return root[x]=find(root[x]);
-    }
-    
-    public void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if(rootX!=rootY) {
-            if(rank[rootX]>rank[rootY]) {
-                root[rootY]=rootX;
-            } else if(rank[rootX]<rank[rootY]) {
-                root[rootX]=rootY;
-            } else {
-                root[rootY]=rootX;
-                rank[rootX]+=1;
-            }
-            components--;
-        }
-    }
-    
-    
     public int minCostConnectPoints(int[][] points) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y)->x[2]-y[2]);
         int n = points.length;
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                int[] px=points[i], py=points[j];
-                var cost = Math.abs(px[0]-py[0])+Math.abs(px[1]-py[1]);
-                pq.add(new int[]{i,j,cost});
-            }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y)->x[2]-y[2]);
+        var start = points[0];
+        for(int i=1; i<n; i++) {
+            var point = points[i];
+            var cost = Math.abs(start[0]-point[0])+Math.abs(start[1]-point[1]);
+            pq.add(new int[]{0,i,cost});
         }
         
-        initialize(n);
-        int totalCost = 0;
+        var visited = new int[n];
+        visited[0]=1;
+        var totalCost = 0;
         
         while(!pq.isEmpty()) {
             var edge = pq.remove();
-            int i=edge[0], j=edge[1], cost=edge[2];
-            if(find(i)!=find(j)) {
-                union(i,j);
-                totalCost+=cost;
+            var j = edge[1];
+            if(visited[j]==0) {
+                visited[j]=1;
+                totalCost+=edge[2];
+                var curr = points[j];
+                for(int i=0; i<n; i++) {
+                    if(visited[i]==0) {
+                        var temp = points[i];
+                        var cost = Math.abs(temp[0]-curr[0])+Math.abs(temp[1]-curr[1]);
+                        pq.add(new int[]{j,i,cost});
+                    }
+                }
             }
         }
+        
         return totalCost;
     }
 }
