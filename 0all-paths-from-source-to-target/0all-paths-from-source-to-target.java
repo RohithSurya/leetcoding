1f361126cd87a06c29b850be5c36211394d6d293
Podugu Rohith Surya <prsurya1020@gmail.com>
2024-02-19 17:09:50 -0800

    Time: 12 ms, Memory: 46.5 MB - LeetHub

--- 0all-paths-from-source-to-target/0all-paths-from-source-to-target.java
+++ 0all-paths-from-source-to-target/0all-paths-from-source-to-target.java
@@ -1,21 +1,25 @@
 class Solution {
     List<List<Integer>> rList = new ArrayList<>();
-    List<List<Integer>> adjList = new ArrayList<>();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        dfs(graph, 0, graph.length-1, new ArrayList<>());
+        var dest = graph.length-1;
+        Set<Integer> visited = new HashSet<>();
+        Queue<List<Integer>> q = new LinkedList<>();
+        q.add(Arrays.asList(0));
+        while(!q.isEmpty()) {
+            var path = q.remove();
+            var element = path.get(path.size()-1);
+            if(element==dest) rList.add(path);
+            visited.add(element);
+                for(var el: graph[element]) {
+                    // if(!visited.contains(el)) {
+                        var nPath = new ArrayList<Integer>(path);
+                        nPath.add(el);
+                        q.add(nPath);
+                    }
+                // }
+        }
         return rList;
     }
     
-    void dfs(int[][] graph, int source, int destination, ArrayList<Integer> cl) {
-        cl.add(source);
-        if(source==destination) 
-            rList.add(new ArrayList<>(cl));
-        else {
-            for(var edge: graph[source]) {
-                dfs(graph, edge, destination, cl);
-            }
-        }
-        cl.remove(cl.size()-1);
-    }
     
 }
